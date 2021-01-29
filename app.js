dd84dbcd737e9fadeb7af5322c0d4064a7db02b7
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-01-30 03:52:54 +0600

    add Destination option

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+// initial Demo Ticket Information and while confirm ticket this format will send to serve
 const ticketInfo = [
 	{
+		from: null,
+		to: null,
+		date: null,
 		id: '1xyz',
 		price: 150,
 		class: 'First Class',
 		quantity: 1,
 	},
 	{
+		from: null,
+		to: null,
+		date: null,
 		id: '2xyz',
 		price: 100,
 		class: 'Economy Class',
@@ -47,19 +54,20 @@ function ticketInfoUpdate(ticketUniqueId, operationType) {
 			if (operationType == 'decrease') {
 				// validate ticket quantity if less then 1 then show some massage
 				if (ticketInfo[i].quantity > 1) {
-					// since  i can't manage  realtime event from dom, by input data in input field that's why i product increases  and multiply  using plus and minus button
+					// since  i can't manage  realtime event from dom, by input data in input field that's why i product increases  and multiply  using plus and minus button event
 					ticketInfo[i].quantity = ticketInfo[i].quantity - 1;
 					subTotalPrice = subTotalPrice - ticketInfo[i].price;
-					// quantity.value = parseInt(quantity.value) - 1;
 
 					// quantity value Direct update in dom
+					// quantity.value = parseInt(quantity.value) - 1;
 					quantity.value = ticketInfo[i].quantity;
 				} else {
-					massage('sorry minimum order 1');
+					massage('<p>Sorry Minimum Order Quantity 1<p/>');
 				}
 			} else {
 				ticketInfo[i].quantity = ticketInfo[i].quantity + 1;
 				subTotalPrice = subTotalPrice + ticketInfo[i].price;
+
 				// quantity value Direct update in dom
 				// quantity.value = parseInt(quantity.value) + 1;
 				quantity.value = ticketInfo[i].quantity;
@@ -83,6 +91,23 @@ document.querySelector('#close').addEventListener('click', function () {
 	document.querySelector('.model').style.display = 'none';
 });
 
+// Destination setup
+function setDestination(destinationType, destination) {
+	for (let i = 0; i < ticketInfo.length; i++) {
+		ticketInfo[i][destinationType] = destination;
+	}
+}
+
+//journey   From
+document.querySelector('#from').addEventListener('keyup', function (e) {
+	setDestination('from', e.target.value);
+});
+
+//journey   To
+document.querySelector('#to').addEventListener('keyup', function (e) {
+	setDestination('to', e.target.value);
+});
+
 // Ticket Increases
 function increasesTicket(ticketUniqueId) {
 	ticketInfoUpdate(ticketUniqueId, 'increases');
@@ -97,13 +122,15 @@ function decreaseTicket(ticketUniqueId) {
 
 // Buy option
 document.querySelector('#buy__btn').addEventListener('click', function () {
-	const mas = `
+	const buyMassage = `
         <div>
         <table class="table table-striped">
         <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">Class</th>
+            <th scope="col">From</th>
+            <th scope="col">To</th>
             <th scope="col">Quantity</th>
             <th scope="col">Price</th>
           </tr>
@@ -112,28 +139,32 @@ document.querySelector('#buy__btn').addEventListener('click', function () {
           <tr>
             <th scope="row">1</th>
             <td>${ticketInfo[0].class}</td>
+            <td>${ticketInfo[0].from}</td>
+            <td>${ticketInfo[0].to}</td>
             <td>${ticketInfo[0].quantity}</td>
             <td>$ ${ticketInfo[0].price}</td>
           </tr>
           <tr>
             <th scope="row">2</th>
             <td>${ticketInfo[1].class}</td>
+            <td>${ticketInfo[0].from}</td>
+            <td>${ticketInfo[0].to}</td>
             <td>${ticketInfo[1].quantity}</td>
             <td>$ ${ticketInfo[1].price}</td>
           </tr>
           <tr>
             <th scope="row"></th>
-            <td colspan="2">Sub Total</td>
+            <td colspan="4">Sub Total</td>
             <td>$ ${subTotalPrice}</td>
           </tr>
           <tr>
             <th scope="row"></th>
-            <td colspan="2">TAX/VAT (10%)</td>
+            <td colspan="4">TAX/VAT (10%)</td>
             <td>$ ${(10 / 100) * subTotalPrice}</td>
           </tr>
           <tr>
             <th scope="row"></th>
-            <td colspan="2">Total</td>
+            <td colspan="4">Total</td>
             <td>$ ${(10 / 100) * subTotalPrice + subTotalPrice}</td>
           </tr>
         </tbody>
@@ -142,6 +173,11 @@ document.querySelector('#buy__btn').addEventListener('click', function () {
       <p>Your Ticket Is Confirmed </p>
         </div>
     `;
-	console.log('e');
-	massage(mas);
+
+	if (ticketInfo[0].from != null && ticketInfo[0].to != null) {
+		massage(buyMassage);
+	} else {
+		massage('<p>Please Choice Your Destination First</p>');
+	}
+	// console.log('error');
 });
